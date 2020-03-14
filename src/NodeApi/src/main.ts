3817c08db23a82ace10496025109777a86834ce0
victormasson <victor.massonv@gmail.com>
2020-03-14 11:53:09 +0100

    Nginx conf
    Add index page for API in Nginx conf.
    Add openAPI for NodeJS

--- src/NodeApi/src/main.ts
+++ src/NodeApi/src/main.ts
@@ -1,12 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   let PORT = process.env.PORT || 80
 
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(PORT);
+
+  const corsOptions: CorsOptions = {
+    // origin: '*',
+    allowedHeaders: '*',
+  }
+  app.enableCors(corsOptions);
+
+
+  const options = new DocumentBuilder()
+    .setTitle('Books example')
+    .setDescription('The bookstore API description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
+  await app.listen(PORT)
 }
 
-bootstrap();
+bootstrap()
